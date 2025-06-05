d93a8a3d9c0dbd84a30b07626f09b0eb8a6eb5fc
Tristan-BS <98057075+Tristan-BS@users.noreply.github.com>
2025-06-05 08:23:36 +0200

    Changelog 05.06.2025 - 08:22
    -> ADDED
         -> Passing create Category values to my manage component
         -> Added Create category Modal form

--- frontend/src/app/modal/create-edit-modal/create-edit-modal.component.ts
+++ frontend/src/app/modal/create-edit-modal/create-edit-modal.component.ts
@@ -1,15 +1,39 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, Input, ViewChild, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-edit-modal',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './create-edit-modal.component.html',
   styleUrl: './create-edit-modal.component.scss'
 })
 export class CreateEditModalComponent {
+
+  @ViewChild('titleInput') titleInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('descInput') descInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('colorInput') colorInput!: ElementRef<HTMLInputElement>;
+
+
+  @Input() mode: string = 'createCategory';
   @Output() close = new EventEmitter<void>();
+  @Output() submit = new EventEmitter<{mode: string, title: string, description: string, color: string}>();
 
   closeModal(): void {
-	this.close.emit();
+	  this.close.emit();
+  }
+
+  submitForm(): void {
+    const title = this.titleInput?.nativeElement.value || '';
+    const description = this.descInput?.nativeElement.value || '';
+    const color = this.colorInput?.nativeElement.value || '';
+
+    console.log(title, description, color);
+
+    this.submit.emit({
+      mode: this.mode,
+      title: title,
+      description: description,
+      color: color
+    });
   }
 }
--- frontend/src/app/pages/manage/manage.component.ts
+++ frontend/src/app/pages/manage/manage.component.ts
@@ -10,12 +10,27 @@ import { CreateEditModalComponent } from '../../modal/create-edit-modal/create-e
 })
 export class ManageComponent {
   isModalVisible = false;
+  modalMode = 'createCategory';
 
-  showModal() {
+  showModal(mode: string = 'createCategory') {
+    this.modalMode = mode;
     this.isModalVisible = true;
   }
 
   hideModal() {
     this.isModalVisible = false;
+  }
+
+  handleModalSubmit(data: {mode: string, title: string, description: string, color: string}) {
+    if (data.mode === 'createCategory') {
+      console.log('Created Successfully --DEBUG');
+      console.log(`Debug Values: \n${data.mode} \n${data.title} \n${data.description} \n${data.color}`);
+    } else if (data.mode === 'editCategory') {
+      console.log('Edited Successfully --DEBUG');
+    } else {
+      console.log('Something went wrong');
+    }
+
+    this.hideModal();
   }
 }
